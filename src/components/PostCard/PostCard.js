f6d3b937e680fb878a2c15f937e08eef59e242e6
Yas Min <yasminlcg@gmail.com>
2022-07-10 13:04:17 +0200

    wip: trick to refresh page after delete post

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -2,52 +2,62 @@ import {Button} from "../Button/Button";
 import {deletePostService} from "../../services/deletePostService";
 import {useState} from "react";
 import {useUserTokenContext} from "../../contexts/UserTokenContext";
+import {useNavigate} from "react-router-dom";
 
-export const PostCard = ({ post }) => {
-  const [error, setError]= useState()
-  const {token}= useUserTokenContext()
-  const {id,title, topic, opening_line, author, text, photo, total_votes,creation_date } = post;
-  const deletePost = async (idPost) => {
-    try {
-      await deletePostService(token, idPost)
+export const PostCard = ({post}) => {
 
-    }catch (error){
-      setError(error.message)
+    const [error, setError] = useState()
+    const {token, idUser} = useUserTokenContext()
+    const {id, title, topic, opening_line, author, text, photo, total_votes, creation_date} = post;
+    const navigate = useNavigate();
+
+    const removePost = async (id) => {
+        try {
+            const response = await deletePostService(token, id)
+            navigate(0);
+            /*quiero que al borrarse un post, recargue userPosts*/
+            /*lifting state up?? */
+
+        } catch (error) {
+            setError(error.message)
+        }
     }
-  }
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    timeZone: "Europe/Madrid",
-  };
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZone: "Europe/Madrid",
+    };
 
-  const localeDate = new Date(creation_date).toLocaleDateString("en-GB", options);
+    const localeDate = new Date(creation_date).toLocaleDateString("en-GB", options);
 
-  return (
-    <section className="entry_info">
-      <header>
-        <p>{topic}</p>
-        <img
-          src={`${process.env.REACT_APP_BASE_URL_IMAGES}/${photo}`}
-          alt={title}
-        />
-        <h2>{title}</h2>
-        <h3>{opening_line}</h3>
-        <p>{text}</p>
-        <p>{localeDate}</p>
-        <h3>{author}</h3>
-        <Button onClick={(e) => {
-          console.log(e)}}>Edit</Button>
-        <Button onClick={() => {
-          console.log("delete post");
-        deletePost(id)}}>Delete</Button>
-      </header>
-      <span>total votes: {total_votes}</span>
+    return (
+        <section className="entry_info">
+            <header>
+                <p>{topic}</p>
+                <img
+                    src={`${process.env.REACT_APP_BASE_URL_IMAGES}/${photo}`}
+                    alt={title}
+                />
+                <h2>{title}</h2>
+                <h3>{opening_line}</h3>
+                <p>{text}</p>
+                <p>{localeDate}</p>
+                <h3>{author}</h3>
+                {(token) && <Button onClick={(e) => {
+                    console.log(e)
+                }}>Edit</Button>}
 
-    </section>
-  );
+                {(token) && <Button onClick={() => {
+                    console.log("delete post");
+                    removePost(id)
+                }}>Delete</Button>}
+            </header>
+            <span>total votes: {total_votes}</span>
+
+        </section>
+    );
 };
--- src/pages/UserPosts/UserPosts.js
+++ src/pages/UserPosts/UserPosts.js
@@ -4,18 +4,30 @@ import {useUserTokenContext} from "../../contexts/UserTokenContext";
 import {useFetchWithDependencyArray} from "../../hooks/useFetchWithDependencyArray";
 import {PostList} from "../../components/PostList/PostList";
 
+
 export const UserPosts = () => {
     const {idUser} = useUserTokenContext()
     const postsByUserEndpoint = getPostsByUser(idUser);
+
+
+   /* const deletePost = (id) => { /!*useEffect??*!/
+        setPosts(allPosts.filter((post) => post.id !== id));
+        console.log(allPosts)
+    };*/
 
     const {data: posts, error} = useFetchWithDependencyArray(postsByUserEndpoint, [idUser]);
     if (error) {
         return <ErrorMessage error={error}/>;
     }
+
     return (
         <section>
             <h2>My posts</h2>
-            {posts && <PostList posts={posts}/>}
+            {idUser && (
+                <>
+            {posts && <PostList posts={posts} />}
+                </>
+            )}
         </section>
     );
 };
